27c3d7c71dfd7c2926ca174d3cb88805ad44be22
Rob Agnese <ragnese@ufl.edu>
2017-01-05 09:52:02 -0500

    Make beer-song much more idiomatic

--- beer-song/src/lib.rs
+++ beer-song/src/lib.rs
@@ -1,21 +1,12 @@
 pub fn sing(start: u32, end: u32) -> String {
-    let mut song = String::new();
-    for i in (end..start+1).rev() {
-        song.push_str(&verse(i));
-        song.push_str("\n");
-    }
-    song.pop();
-    song
+    (end..start+1).rev().map(|x| verse(x)).collect::<Vec<_>>().join("\n") 
 }
 
 pub fn verse(n: u32) -> String {
-    if n == 0 {
-        "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".to_string()
-    } else if n == 1 {
-        "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n".to_string()
-    } else if n == 2 {
-        "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n".to_string()
-    } else {
-        format!("{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottles of beer on the wall.\n", n, n, n-1)
+    match n {
+        0 => "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".to_string(),
+        1 => "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n".to_string(),
+        2 => "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n".to_string(),
+        _ => format!("{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottles of beer on the wall.\n", n, n, n-1),
     }
 }
