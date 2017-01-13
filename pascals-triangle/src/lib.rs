c52295a84876bd0ea7a763e07b35dc2e8d2934bc
Rob Agnese <ragnese@ufl.edu>
2017-01-12 21:58:06 -0500

    Way better solution for pascals-triangle

--- pascals-triangle/src/lib.rs
+++ pascals-triangle/src/lib.rs
@@ -8,25 +8,15 @@ impl PascalsTriangle {
     }
 
     pub fn rows(&self) -> Vec<Vec<u32>> {
-        match self.row_count {
-            0 => Vec::new(),
-            _ => (1..self.row_count).fold(vec![vec![1]], |res, _| append_next_row(res)),
-        }
+        (0..self.row_count).map(|x| calc_row(x)).collect()
     }
 }
 
-fn append_next_row(mut rows: Vec<Vec<u32>>) -> Vec<Vec<u32>> {
-    let end_row = rows.last().unwrap().to_vec();
-    let end_row_iter_left = end_row[0..end_row.len() - 1].iter();
-    let end_row_iter_right = end_row[1..end_row.len()].iter();
-
-    let mut next_row: Vec<u32> = end_row_iter_left.zip(end_row_iter_right)
-        .fold(vec![1], |mut res, (x, y)| {
-            res.push(x + y);
-            res
-        });
-    next_row.push(1);
-
-    rows.push(next_row);
-    rows
+fn calc_row(n: u32) -> Vec<u32> {
+    let mut next_row: Vec<u32> = Vec::with_capacity(n as usize + 1);
+    (1..n + 2).fold(1, |acc, x| {
+        next_row.push(acc);
+        acc * (n + 1 - x) / x
+    });
+    next_row
 }
